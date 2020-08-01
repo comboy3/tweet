0db93f704bb8c3d46bd5e9944298ab6d1f7e87f8
KONKON <taba3taba3taba3@gmail.com>
2020-08-02 00:19:03 +0900

    chapter4
    pull

--- tweetsite/settings.py
+++ tweetsite/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tweet',
+    'user',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
